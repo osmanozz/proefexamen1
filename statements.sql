36adb4058e6ae053fef0ebf75f54f01a2113629e
osmanozz <2120601@talnet.nl>
2021-09-24 14:44:00 +0200

    first

--- statements.sql
+++ statements.sql
@@ -2,6 +2,14 @@
 CREATE DATABASE catchmorefish;
 USE catchmorefish;
 
+CREATE table amsterdam(
+    productcode int AUTO_INCREMENT,
+    naam varchar(255) NOT NULL,
+    inkoop_waarde decimal(6,2) NOT NULL,
+    verkoop_waarde decimal(6,2) NOT NULL,
+    PRIMARY KEY (productcode)
+);
+
 CREATE TABLE medewerker(
     id INT AUTO_INCREMENT,
     naam varchar(255),
@@ -9,25 +17,5 @@ CREATE TABLE medewerker(
     password varchar(255) not null,
     PRIMARY KEY (id)
 );
-CREATE table amsterdam(
-    id_ams int AUTO_INCREMENT,
-    naam varchar(255) NOT NULL,
-    prijs_per_stuk decimal(6,2) NOT NULL,
-    voorraad int(10),
-    PRIMARY KEY (id_ams)
-);
-CREATE table rotterdam(
-    id_rot INT AUTO_INCREMENT,
-    naam varchar(255) NOT NULL,
-    prijs_per_stuk decimal(6,2) NOT NULL,
-    voorraad int(10),
-    PRIMARY KEY (id_rot)
-);
-CREATE table zoetermeer(
-    id_zoeter INT AUTO_INCREMENT,
-    naam varchar(255) NOT NULL,
-    prijs_per_stuk decimal(6,2) NOT NULL,
-    voorraad int(10),
-    PRIMARY KEY (id_zoeter)
-);
+
 
